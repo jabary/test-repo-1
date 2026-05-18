7123497a9ca8be14763a095a556f4be4cd94f3ca
Mohammed Jabari <jabary980@ppu.edu>
2026-05-18 10:26:44 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+	<?php
+
+	for($i=0;$i<10;$i++){
+		echo "<h5>Hello World</h5>";
+	}
+
+
+
+	?>
+
+</body>
+</html>
